aa26a3c99c92f0fd8cfe4ab0de3a8fd5b54066c6
guylainecastellano <guylaine.a@wanadoo.fr>
2020-05-12 11:45:47 +0200

    ex6 partie4 php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>ex6 partie4 php</title>
+</head>
+<body>
+  <?php
+  for ($numbers = 1; $numbers <= 100; $numbers++){
+    $restDivision = $numbers % 3;
+    $result = $numbers;
+    $division5 = $numbers % 5;
+    if ($restDivision == 0){
+      $result = 'Fizz';
+    }
+    if ($division5 == 0){
+      $result = 'Buzz';
+    }
+    if ($restDivision == 0 && $division5 == 0){
+      $result = 'FizzBuzz';
+    }
+    ?>
+    <p><?php echo $result; ?></p>
+  <?php } ?>
+</body>
+</html>
